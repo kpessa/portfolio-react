b7ab817c80d04fc8810853bd49689af358a151d5
Kurt Pessa <kpessa@gmail.com>
2021-05-20 22:15:12 -0400

    worked on hamburger toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
+
 import './Navbar.css';
 import logo from '../images/logo.png';
 
 function Navbar({ currentPage, setCurrentPage }) {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="nav">
       <a href="#" className="nav__logo">
         <img src={logo} alt="" />
         <h1>KP</h1>
       </a>
-      <ul className="nav__links">
+      <ul className={`nav__links ${toggle && 'open'}`}>
         <li>
           <a href="#about-me" onClick={() => setCurrentPage('about-me')}>
             About Me
@@ -25,7 +29,7 @@ function Navbar({ currentPage, setCurrentPage }) {
           </a>
         </li>
       </ul>
-      <svg viewBox="0 0 100 80" width="40" height="40">
+      <svg onClick={() => setToggle(!toggle)} viewBox="0 0 100 80" width="40" height="40">
         <rect width="100" height="15"></rect>
         <rect y="30" width="100" height="15"></rect>
         <rect y="60" width="100" height="15"></rect>
