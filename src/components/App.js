aa9540c5c4a79f53532bf04d626810404c1f2d8e
Kurt Pessa <kpessa@gmail.com>
2022-02-22 10:12:15 -0500

    added links

--- src/components/App.js
+++ src/components/App.js
@@ -12,37 +12,37 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('');
+	const [currentPage, setCurrentPage] = useState('');
 
-  const renderPage = () => {
-    const map = new Map();
-    map.set('about-me', <AboutMe />);
-    map.set('portfolio', <Portfolio />);
-    map.set('resume', <Resume />);
-    map.set('contact', <Contact />);
+	const renderPage = () => {
+		const map = new Map();
+		map.set('about-me', <AboutMe />);
+		map.set('portfolio', <Portfolio />);
+		// map.set('resume', <Resume />);
+		map.set('contact', <Contact />);
 
-    return map.get(currentPage);
-  };
+		return map.get(currentPage);
+	};
 
-  const jumbotron = (
-    <section className="hero-jumbotron">
-      <h1>Hi, I'm Kurt ;)</h1>
-      <p>I am a full-stack web developer, pharmacist and data analyst who enjoys developing innovative products</p>
-    </section>
-  );
+	const jumbotron = (
+		<section className="hero-jumbotron">
+			<h1>Hi, I'm Kurt ;)</h1>
+			<p>I am a full-stack web developer, pharmacist and data analyst who enjoys developing innovative products</p>
+		</section>
+	);
 
-  return (
-    <div className="wrapper">
-      <header>
-        <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      </header>
-      <main>
-        {!currentPage && jumbotron}
-        {renderPage()}
-      </main>
-      <Footer currentPage={currentPage} />
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<header>
+				<Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+			</header>
+			<main>
+				{!currentPage && jumbotron}
+				{renderPage()}
+			</main>
+			<Footer currentPage={currentPage} />
+		</div>
+	);
 }
 
 export default App;
